84afbd30545b1a0cae2f3f2e28ac4faf036cc031
ScruffyC <118226061+ScruffyC@users.noreply.github.com>
2022-11-22 16:57:49 -0600

    connecting to db

--- src/main/java/com/revature/ers/models/User.java
+++ src/main/java/com/revature/ers/models/User.java
@@ -3,18 +3,26 @@ package com.revature.ers.models;
 public class User {
     private String id;
     private String username;
+    private String email;
     private String password;
-    private String role;
+    private String givenName;
+    private String surname;
+    private Boolean isActive;
+    private String roleID;
 
     public User() {
         super();
     }
 
-    public User(String id, String username, String password, String role) {
+    public User(String id, String username, String email, String password, String givenName, String surname, Boolean isActive, String roleID) {
         this.id = id;
         this.username = username;
+        this.email = email;
         this.password = password;
-        this.role = role;
+        this.givenName = givenName;
+        this.surname = surname;
+        this.isActive = isActive;
+        this.roleID = roleID;
     }
 
     public String getId() {
@@ -25,6 +33,22 @@ public class User {
         this.id = id;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
     }
@@ -33,12 +57,36 @@ public class User {
         this.password = password;
     }
 
-    public String getRole() {
-        return role;
+    public String getGivenName() {
+        return givenName;
     }
 
-    public void setRole(String role) {
-        this.role = role;
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    public String getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(String roleID) {
+        this.roleID = roleID;
     }
 
     @Override
@@ -46,8 +94,12 @@ public class User {
         return "User{" +
                 "id='" + id + '\'' +
                 ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
-                ", role='" + role + '\'' +
+                ", givenName='" + givenName + '\'' +
+                ", surname='" + surname + '\'' +
+                ", isActive=" + isActive +
+                ", roleID='" + roleID + '\'' +
                 '}';
     }
 }
